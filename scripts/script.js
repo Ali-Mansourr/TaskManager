4ceb10b63a51721543ea9b4a6757430810dce608
manso21 <alimanso234@gmail.com>
2023-11-10 23:26:36 +0200

    Final touches

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () =>{
     const form = document.querySelector("#new-task-form");
-    const input = document.querySelector("#new-task");
+    const input = document.querySelector("#task-input");
     const list_el = document.querySelector("#tasks");
 
     form.addEventListener('submit', (e) => {
@@ -24,7 +24,7 @@ window.addEventListener('load', () =>{
         const task_input_el = document.createElement("input");
         task_input_el.classList.add("text");
         task_input_el.type = "text";
-        task.task_input_el.value = task;
+        task_input_el.value = task;
         task_input_el.setAttribute("readonly", "readonly");
 
         task_content_el.appendChild(task_input_el);
